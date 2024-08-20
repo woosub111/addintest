47629b3a4bf03597659938ff93a65a3a9b8e1189
woosub111 <159146591+woosub111@users.noreply.github.com>
2024-08-20 11:16:27 +0900

    Add files via upload

---
+++ FunctionFile.js
@@ -0,0 +1,16 @@
+﻿Office.initialize = function () {
+}
+
+// 정보 표시줄에 상태 메시지를 추가하는 도우미 함수입니다.
+function statusUpdate(icon, text) {
+  Office.context.mailbox.item.notificationMessages.replaceAsync("status", {
+    type: "informationalMessage",
+    icon: icon,
+    message: text,
+    persistent: false
+  });
+}
+
+function defaultStatus(event) {
+  statusUpdate("icon16" , "Hello World!");
+}
